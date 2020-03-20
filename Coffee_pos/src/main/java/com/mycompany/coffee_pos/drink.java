0b20aae42a1023bbb379dccf3908fe3292a18668
นายธนัท ภูระบัตร <58841345+613410068-9@users.noreply.github.com>
2020-03-20 20:58:09 +0700

    PNFO_Cafe
    Member
    นายธนัท ภูรับตร 613410068-9
    นางสาวสุธีกานต์ ภูศักดิ์ 613410071-0
    นายศุภกร คันธโกวิท 613410092-2
    นายสรวิศ พิสัยพันธ์ 613410093-0

---
+++ Coffee_pos/src/main/java/com/mycompany/coffee_pos/drink.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.coffee_pos;
+
+/**
+ * 
+ * @author 66982
+ */
+public class drink {
+   //public int mn (int sum_menu){
+    //return "Hot Latte\t"+Integer.valueOf(int bt_lattehot.getText())+"\tBath\n\r";
+    
+//}
+}
